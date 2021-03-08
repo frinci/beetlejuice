1036416c569e669cf1046987e103b8c9affc47b3
Frenchy French <frenchy.french@a-c02dqevbmd6m.home>
2021-03-08 15:42:48 -0500

    added sdk

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1615234807.3541255
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://beetlejuice.netlify.app/images/beetlejuiceLogo.png',
+  defaultTitle: 'Beetlejuice the Bio\u002DExorcist!',
+  defaultActionURL: 'https://beetlejuice.netlify.app',
+  appKey: 'RpumHKzORpWgFV4jhAlseQ',
+  token: 'MTpScHVtSEt6T1JwV2dGVjRqaEFsc2VROm9GMHZnOUh1TzFwRjRtLW9DSlZhRk85NGc5c0xZM3VGZHVjTmtKQUFIZFk',
+  vapidPublicKey: 'BATXCxb8dsKhiwkV-l1AV9L9p938Zr0Yx1I0lKYpxymZ5_YIdPeSLEHJ-bIG5jYpl7uumrXP-s7An437uPzW7iA='
+})
